0bda5853b171ea6933bd18b406ac5f5ecf1dc632
BoYoung00 <101164065+BoYoung00@users.noreply.github.com>
2022-11-08 22:59:48 +0900

    백엔드 공부 7일차??

--- src/main/java/com/example/back_java_study1/HelloServlet.java
+++ src/main/java/com/example/back_java_study1/HelloServlet.java
@@ -1,27 +1,30 @@
 package com.example.back_java_study1;
 
-import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+//1단계 Servlet (프론트 -- 서블릿 -- 백엔드)
+
+
+//맵핑
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    private static final long seriaVersionUID=1L;
 
-    public void init() {
-        message = "Hello World!";
+    public HelloServlet(){
+        super();
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
+//    받고, 보내기, 오류 날 경우 입센션 처리
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws SecurityException, IOException {
+        response.setContentType("text/html; charset=utf-8");
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
+        out.println("<h1>1~10까지 출력</h1>");
+        for (int i=0; i<10; i++) {
+            out.println(i + "<br>");
+        }
+        out.close();
     }
 }
